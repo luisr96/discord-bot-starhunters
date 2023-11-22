7285ff4edb89321a800258bf1d11e6e4bbd97f21
Luis <luisramirez9696@gmail.com>
2023-11-22 00:12:56 -0500

    Refactor to builder pattern Add locations field and data

--- commands/manual-call.js
+++ commands/manual-call.js
@@ -4,42 +4,41 @@ const logger = pino({
     target: "pino-pretty",
   },
 });
-const {
-  ApplicationCommandOptionWithChoicesAndAutocompleteMixin,
-} = require("discord.js");
+const locations = require("../data/locations.json");
+const { SlashCommandBuilder } = require("discord.js");
 
-module.exports = {
-  data: {
-    name: "manual-call",
-    description: "Manually call a star",
-    options: [
-      {
-        name: "world",
-        description: "World number",
-        type: 3,
-        required: true,
-      },
-      {
-        name: "tier",
-        description: "Tier of the star",
-        type: 3,
-        required: true,
-      },
-    ],
-  },
+const data = new SlashCommandBuilder()
+  .setName("manual-call")
+  .setDescription("Call a star manually")
+  .addStringOption((option) =>
+    option
+      .setName("world")
+      .setDescription("The world number of the star")
+      .setRequired(true)
+  )
+  .addStringOption((option) =>
+    option
+      .setName("tier")
+      .setDescription("The tier of the star")
+      .setRequired(true)
+  )
+  .addStringOption((option) =>
+    option
+      .setName("location")
+      .setDescription("The location of the star")
+      .setRequired(true)
+  );
+locations.forEach((loc) => {
+  data.options[2].addChoices(loc);
+});
 
-  run: ({ interaction, client, handler }) => {
-    const world = interaction?.options._hoistedOptions[0].value;
-    const tier = interaction?.options._hoistedOptions[1].value;
-    logger.info(`T${tier} STAR | W${world} | BY: ${interaction.user.username}`);
-    interaction.reply("Star recorded.");
-  },
-
-  options: {
-    devOnly: true,
-    guildOnly: true,
-    userPermissions: ["Administrator", "AddReactions"],
-    botPermissions: ["Administrator", "AddReactions"],
-    deleted: false,
-  },
-};
+function run({ interaction }) {
+  const world = interaction.options.getString("world");
+  const tier = interaction.options.getString("tier");
+  const location = interaction.options.getString("location");
+  logger.info(
+    `W${world} T${tier} STAR | ${location} | BY: ${interaction.user.username}`
+  );
+  interaction.reply("Star recorded.");
+}
+module.exports = { data, run };
