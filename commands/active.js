cac3809db861fc4b09256e22613afd339278c5f5
Martin <martingreen@outlook.com>
2024-06-05 23:10:22 -0700

    Estimate star tier based on updated time and add docker compose for easier development hot reloading

--- commands/active.js
+++ commands/active.js
@@ -35,6 +35,11 @@ async function run({ interaction }) {
         const foundDate = new Date(star.foundAt);
         const updatedDate = new Date(star.updatedAt);
 
+        // Estimate current tier based on last updated time
+        let lastUpdate = star.updatedAt ? updatedDate : foundDate;
+        let tierDiff = parseInt((new Date() - lastUpdate) / (1000 * 60 * 7));
+        let estimatedTier = Math.max(star.tier - tierDiff, 0);
+
         totalWorld = "";
         if (totalWorldList[0]["500 total worlds"].includes(star.world)) {
           totalWorld = "(500 total)";
@@ -45,10 +50,8 @@ async function run({ interaction }) {
         embed.addFields({
           name: `⭐ ${star.location}`,
           value: `World: ${star.world} ${totalWorld}
-                  Tier: ${star.tier}
-                  Found ${formatDistanceToNow(foundDate, {
-                    addSuffix: true,
-                  })} by <@${star.foundBy}>
+                  Tier: ${estimatedTier}${estimatedTier == star.tier ? '' : '*'}
+                  Found ${formatDistanceToNow(foundDate, { addSuffix: true, })} by <@${star.foundBy}>
                   ${star.updatedAt ? 'Updated ' + formatDistanceToNow(updatedDate, { addSuffix: true, }) : ''}
                   `,
         });
