4a5f28cae03b356dedaba51b2a1a92f5e3471882
Martin <martingreen@outlook.com>
2024-06-22 18:09:48 -0700

    Auto send active list and delete previous messages in a bot-only channel when it is restarted.

--- commands/active.js
+++ commands/active.js
@@ -22,79 +22,7 @@ async function run({ c, client, interaction }) {
   await interaction.deferReply();
 
   const starsEmbed = await activeStarsEmbed();
-
   interaction.editReply(starsEmbed);
 
-  // channel.send(starsEmbed);
-
-
-  // try {
-  //   const starsCollection = db.getStarsCollection();
-
-  //   const channel = client.channels.cache.get('1199112511048388701');
-
-  //   // console.log("\n\nc: ", c);
-  //   // console.log("\n\nclient: ", client);
-  //   // console.log("\n\ninteraction: ", interaction);
-
-  //   // Defer the reply to ensure enough time to process the command
-  //   // await interaction.deferReply();
-
-  //   const releasedStars = await starsCollection
-  //     .find({ calledAt: { $exists: true, $ne: null } })
-  //     .sort({ foundAt: 1 })
-  //     .toArray();
-
-  //   if (releasedStars.length > 0) {
-  //     const embed = new EmbedBuilder()
-  //       .setTitle("Released Stars")
-  //       .setColor("#00ff00");
-
-  //     releasedStars.forEach((star, index) => {
-  //       const foundDate = new Date(star.foundAt);
-  //       const updatedDate = new Date(star.updatedAt);
-
-  //       // Estimate current tier based on last updated time
-  //       let lastUpdate = star.updatedAt ? updatedDate : foundDate;
-  //       let tierDiff = parseInt((new Date() - lastUpdate) / (1000 * 60 * 7));
-  //       let estimatedTier = Math.max(star.tier - tierDiff, 0);
-
-  //       totalWorld = "";
-  //       if (totalWorldList[0]["500 total worlds"].includes(star.world)) {
-  //         totalWorld = "(500 total)";
-  //       } else if (totalWorldList[0]["750 total worlds"].includes(star.world)) {
-  //         totalWorld = "(750 total)";
-  //       }
-
-  //       embed.addFields({
-  //         name: `⭐ ${star.location}`,
-  //         value: `World: ${star.world} ${totalWorld}
-  //                 Tier: ${estimatedTier}${estimatedTier == star.tier ? '' : '*'}
-  //                 Found ${formatDistanceToNow(foundDate, { addSuffix: true, })} by <@${star.foundBy}>
-  //                 ${star.updatedAt ? 'Updated ' + formatDistanceToNow(updatedDate, { addSuffix: true, }) : ''}
-  //                 `,
-  //       });
-  //     });
-  //     logger.info("/active");
-  //     channel.send({ embeds: [embed] });
-  //     // interaction.editReply({ embeds: [embed] });
-  //   } else {
-  //     channel.send({ content: "No active stars found" });
-  //     // interaction.followUp("No active stars found");
-  //   }
-  // } catch (error) {
-  //   console.error("Error querying MongoDB:", error);
-  //   channel.send({ content: "Error: Could not get active stars" });
-  //   // interaction.followUp("Error: Could not get active stars");
-  // }
-
-  //     interaction.editReply({ embeds: [embed] });
-  //   } else {
-  //     interaction.followUp("No active stars found");
-  //   }
-  // } catch (error) {
-  //   console.error("Error querying MongoDB:", error);
-  //   interaction.followUp("Error: Could not get active stars");
-  // }
 }
 module.exports = { data, run };
--- events/ready/console-log.js
+++ events/ready/console-log.js
@@ -14,86 +14,22 @@ const { format, formatDistanceToNow, parseISO } = require("date-fns");
 
 
 module.exports = async (c, client, interaction) => {
-  // async function run() {
-  // module.exports = (c, client, handler) => {
-  console.log(`${c.user.username} is ready!`);
-
-  // console.log("c: ", c);
-  // console.log("client: ", client);
-  console.log("interaction: ", interaction);
-
-  const channel = client.channels.cache.get('1199112511048388701');
-  // console.log(channel);
-  channel.send({ content: "Test message" });
-
-  // console.log("\n\n\n Client: ", client);
-  // console.log("\n\n\n handler: ", handler);
+  const channel = client.channels.cache.get('1254231419497091194');
+  channel.send({ content: "Bot is restarting..." });
 
 
   const starsEmbed = await activeStarsEmbed();
 
+  // setTimeout(() => {
+  // console.log("\n\nLast Message:\n", channel.lastMessageId);
+
+  channel.bulkDelete(5)
+    .then(messages => console.log(`Removed ${messages.size} messages`))
+    .catch(console.error);
+
   channel.send(starsEmbed);
 
-
-
-  // try {
-  //   const starsCollection = db.getStarsCollection();
-
-  //   // Defer the reply to ensure enough time to process the command
-  //   // await interaction.deferReply();
-
-  //   const releasedStars = await starsCollection
-  //     .find({ calledAt: { $exists: true, $ne: null } })
-  //     .sort({ foundAt: 1 })
-  //     .toArray();
-
-  //   if (releasedStars.length > 0) {
-  //     const embed = new EmbedBuilder()
-  //       .setTitle("Released Stars")
-  //       .setColor("#00ff00");
-
-  //     releasedStars.forEach((star, index) => {
-  //       const foundDate = new Date(star.foundAt);
-  //       const updatedDate = new Date(star.updatedAt);
-
-  //       // Estimate current tier based on last updated time
-  //       let lastUpdate = star.updatedAt ? updatedDate : foundDate;
-  //       let tierDiff = parseInt((new Date() - lastUpdate) / (1000 * 60 * 7));
-  //       let estimatedTier = Math.max(star.tier - tierDiff, 0);
-
-  //       totalWorld = "";
-  //       if (totalWorldList[0]["500 total worlds"].includes(star.world)) {
-  //         totalWorld = "(500 total)";
-  //       } else if (totalWorldList[0]["750 total worlds"].includes(star.world)) {
-  //         totalWorld = "(750 total)";
-  //       }
-
-  //       embed.addFields({
-  //         name: `⭐ ${star.location}`,
-  //         value: `World: ${star.world} ${totalWorld}
-  //                 Tier: ${estimatedTier}${estimatedTier == star.tier ? '' : '*'}
-  //                 Found ${formatDistanceToNow(foundDate, { addSuffix: true, })} by <@${star.foundBy}>
-  //                 ${star.updatedAt ? 'Updated ' + formatDistanceToNow(updatedDate, { addSuffix: true, }) : ''}
-  //                 `,
-  //       });
-  //     });
-  //     logger.info("/console-log");
-  //     channel.send({ embeds: [embed] });
-  //     // interaction.editReply({ embeds: [embed] });
-  //   } else {
-  //     channel.send({ content: "No active stars found" });
-  //     // interaction.followUp("No active stars found");
-  //   }
-  // } catch (error) {
-  //   console.error("Error querying MongoDB:", error);
-  //   channel.send({ content: "Error: Could not get active stars" });
-  //   // interaction.followUp("Error: Could not get active stars");
-  // }
+  // }, 0);
 
 
 };
-
-
-// module.exports = { run };
-
-// module.exports = (c, client, handler) => await {
